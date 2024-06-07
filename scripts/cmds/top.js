c411b166cc816727aa2e89f27198d6ada82506ef
Ban-05 <igoat149@gmail.com>
2024-06-07 21:19:17 +0000

    Update top.js
    Signed-off-by: Ban-05 <igoat149@gmail.com>

--- scripts/cmds/top.js
+++ scripts/cmds/top.js
@@ -5,7 +5,7 @@ module.exports = {
  author: "Loid Butter",
  role: 0,
  shortDescription: {
- en: " ❥ʚ𝑬𝑷𝑯𝑬𝑵𝑰𝑨๖ۣ•҉ 𝐓𝐨𝐩 100 𝐑𝐢𝐜𝐡 𝐔𝐬𝐞𝐫𝐬"
+ en: " ♚𝐋𝐢𝐦𝐮𝐥𝐞♚ 𝐓𝐨𝐩 100 𝐑𝐢𝐜𝐡 𝐔𝐬𝐞𝐫𝐬"
  },
  longDescription: {
  en: ""
@@ -22,7 +22,7 @@ module.exports = {
  
  const topUsersList = topUsers.map((user, index) => `${index + 1}. ${user.name}: ${user.money}`);
  
- const messageText = ` ❥𝐒𝐎𝐍𝐈𝐂🍀 𝐓𝐎𝐏 𝐑𝐈𝐂𝐇𝐄𝐒𝐓 🎁💰\n━━━━━━━━━━━━\n${topUsersList.join('\n')}`;
+ const messageText = ` 💧𝑳𝒊𝒎𝒖𝒍𝒆 💧 𝐓𝐎𝐏 𝐑𝐈𝐂𝐇𝐄𝐒𝐓 💧\n━━━━━━━━━━━━\n${topUsersList.join('\n')}`;
  
  message.reply(messageText);
  }
